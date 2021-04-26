056ee1ffa0ba08be6d5409fe7ad852956c91dcbf
BuildTools <unconfigured@null.spigotmc.org>
2021-04-27 03:11:23 +0500

    edited config.py

--- data/config.py
+++ data/config.py
@@ -1,14 +1,4 @@
-from environs import Env
+import os
 
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
-env = Env()
-env.read_env()
-
-BOT_TOKEN = env.str("TOKEN")  # Забираем значение типа str
-# ADMINS = {784007452, 374927548, 484085985}
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-#IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
-USER = env.str("USER")
-PASS = env.str("PASS")
-host = env.str("host")
-db_name = env.str("db")
+BOT_TOKEN = os.environ['TOKEN']
+ADMINS = os.environ['ADMINS']
